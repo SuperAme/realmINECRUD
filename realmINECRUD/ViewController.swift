7653fe7e78ee2d7132ead2b823e3e099cc3f73cb
Américo MQ <americo.meneses.contractor@bbva.com>
2021-02-11 18:03:17 -0600

    first commit

---
+++ realmINECRUD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realmINECRUD
+//
+//  Created by IDS Comercial on 11/02/21.
+//  Copyright © 2021 Americo MQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
